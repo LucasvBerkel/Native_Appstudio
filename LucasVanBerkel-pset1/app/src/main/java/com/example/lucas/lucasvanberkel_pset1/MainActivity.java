0d86c3d33442606051366969ec4b974327fe2f94
Lucas <lucasvberkel@gmail.com>
2016-11-01 15:07:11 +0100

    Hello

--- LucasVanBerkel-pset1/app/src/main/java/com/example/lucas/lucasvanberkel_pset1/MainActivity.java
+++ LucasVanBerkel-pset1/app/src/main/java/com/example/lucas/lucasvanberkel_pset1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lucas.lucasvanberkel_pset1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onCheckboxClicked(View view){
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()){
+            case R.id.BodyBox:
+                if(checked){
+                    changeVisibility(findViewById(R.id.body), "VISIBLE");
+                } else {
+                    changeVisibility(findViewById(R.id.body), "INVISIBLE");
+                }
+                break;
+        }
+    }
+
+    public void changeVisibility(View view, String visibility){
+        if(visibility == "VISIBLE"){
+            view.setVisibility(View.VISIBLE);
+        } else {
+            view.setVisibility(View.INVISIBLE);
+        }
     }
 }
